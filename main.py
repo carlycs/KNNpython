3439219d6d7000058f1962b82f57261f3a0697ce
Hoofar Pourzand <hp@lotustech.io>
2016-05-02 12:36:26 -0400

    data read fix

--- main.py
+++ main.py
@@ -2,30 +2,35 @@ import csv
 import random
 import math
 import operator
-from loadDataset.py import loadDataset 
-from euclideanDistance.py import euclideanDistance  
-from getNeighbors.py import getNeighbors   
-from getResponse.py import getResponse   
-from getAccuracy.py import getAccuracy  
+#append directories to python path
+import sys
+sys.path.append("/home/fixinthis/Documents/git/KNNpython")
+
+from loadDataset import loadDataset 
+from euclideanDistance import euclideanDistance  
+from getNeighbors import getNeighbors   
+from getResponse import getResponse   
+from getAccuracy import getAccuracy  
  
 def main():
-	# prepare data
-	trainingSet=[]
-	testSet=[]
-	split = .80
-	loadDataset('finaldata-data01.csv', split, trainingSet, testSet)
-	print 'Train set: ' + repr(len(trainingSet))
-	print 'Test set: ' + repr(len(testSet))
-	# generate predictions
-	predictions=[]
-	k = 3
-	for x in range(len(testSet)):
-		neighbors = getNeighbors(trainingSet, testSet[x], k)
-		result = getResponse(neighbors)
-		predictions.append(result)
-		print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
-	accuracy = getAccuracy(testSet, predictions)
-	print('Accuracy: ' + repr(accuracy) + '%')
+    # prepare data
+    trainingSet=[]
+    testSet=[]
+    split = 0.80
+    loadDataset('crime.csv', split, trainingSet, testSet)
+    print('Train set: ' + repr(len(trainingSet)))
+    print('Test set: ' + repr(len(testSet)))
+    # generate predictions
+    predictions=[]
+    k = 3
+    for x in range(len(testSet)):
+            neighbors = getNeighbors(trainingSet, testSet[x], k)
+            result = getResponse(neighbors)
+            predictions.append(result)
+            print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: ' + repr(accuracy) + '%')
 main()
 
 
